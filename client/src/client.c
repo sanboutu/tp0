d539f74aa2c9c992db7358f5d774937c9f40111f
sanboutu <sperezboutureira@frba.utn.edu.ar>
2025-04-01 17:11:22 +0000

    Parte 1 y Parte 2

--- client/src/client.c
+++ client/src/client.c
@@ -16,19 +16,33 @@ int main(void)
 
 	logger = iniciar_logger();
 
+	if( logger == NULL ) {
+		printf("Error en log");
+		abort();
+	}
+
 	// Usando el logger creado previamente
 	// Escribi: "Hola! Soy un log"
+	log_info(logger, "Hola! Soy un log");
 
 
 	/* ---------------- ARCHIVOS DE CONFIGURACION ---------------- */
 
 	config = iniciar_config();
 
+	if( config == NULL ) {
+		printf("Error en config");
+		abort();
+	}
+
 	// Usando el config creado previamente, leemos los valores del config y los 
 	// dejamos en las variables 'ip', 'puerto' y 'valor'
+	ip	   = config_get_string_value(config, "IP");
+	puerto = config_get_string_value(config, "PUERTO");
+	valor  = config_get_string_value(config, "CLAVE");
 
 	// Loggeamos el valor de config
-
+	log_info(logger, "Valor de config: %s",valor);
 
 	/* ---------------- LEER DE CONSOLA ---------------- */
 
@@ -54,14 +68,14 @@ int main(void)
 
 t_log* iniciar_logger(void)
 {
-	t_log* nuevo_logger;
-
+	t_log* nuevo_logger = log_create("tp0.log", "LOGGER_TP0", 1, LOG_LEVEL_INFO);
+	
 	return nuevo_logger;
 }
 
 t_config* iniciar_config(void)
 {
-	t_config* nuevo_config;
+	t_config* nuevo_config = config_create("cliente.config");
 
 	return nuevo_config;
 }
@@ -74,10 +88,14 @@ void leer_consola(t_log* logger)
 	leido = readline("> ");
 
 	// El resto, las vamos leyendo y logueando hasta recibir un string vacío
-
-
+	while( strcmp(leido, "") ) {
+		log_info(logger, "%s", leido);
+		free(leido);
+		leido = readline("> ");
+	}
+	
 	// ¡No te olvides de liberar las lineas antes de regresar!
-
+	free(leido);
 }
 
 void paquete(int conexion)
@@ -97,4 +115,6 @@ void terminar_programa(int conexion, t_log* logger, t_config* config)
 {
 	/* Y por ultimo, hay que liberar lo que utilizamos (conexion, log y config) 
 	  con las funciones de las commons y del TP mencionadas en el enunciado */
+	log_destroy(logger);
+	config_destroy(config);
 }
